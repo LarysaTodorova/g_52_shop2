cc9e65a948974627e10d5d517a8fbdf2fb2ca90c
Lora <lorageo33@gmail.com>
2026-04-01 20:05:42 +0300

    initial commit

---
+++ src/main/java/de/aittr/g_52_shop2/G52Shop2Application.java
@@ -0,0 +1,13 @@
+package de.aittr.g_52_shop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G52Shop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G52Shop2Application.class, args);
+	}
+
+}
