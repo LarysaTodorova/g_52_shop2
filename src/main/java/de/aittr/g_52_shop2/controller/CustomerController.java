4ea6a50d55b6213168d211d03ca54ce2735e5c9a
Lora <lorageo33@gmail.com>
2026-04-02 21:07:53 +0300

    initial commit

---
+++ src/main/java/de/aittr/g_52_shop2/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package de.aittr.g_52_shop2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+}
