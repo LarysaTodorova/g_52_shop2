a72c37be22fb0ec2e2c77e08461e1454eda9efe4
Lora <lorageo33@gmail.com>
2026-04-23 21:43:35 +0300

    initial commit

--- src/main/java/de/aittr/g_52_shop2/security/AuthInfo.java
+++ src/main/java/de/aittr/g_52_shop2/security/AuthInfo.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
